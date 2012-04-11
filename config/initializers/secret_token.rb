f116ecc9443ab0c6acff07b14ce4a0ca58d21e11
visnupriya <visnupriyar@spritle.com>
2012-04-11 16:49:21 +0530

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mydemoapp::Application.config.secret_token = '5f3a3c27ed35df9cbc1c09a56ac64ee8271d507ca329facf02bfb13c6522f37980f3cc290433cf0179949cca6934e4b47526595a47532a6d21993601f77f41c2'
